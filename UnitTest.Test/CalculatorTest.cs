3eaec695b53deb316d28da7b25e61dc6ef967117
Göksel Karadağ <goksel.karadag34@gmail.com>
2024-11-23 17:44:36 +0300

    Add project files.

---
+++ UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.App;
+
+namespace UnitTest.Test
+{
+    public class CalculatorTest
+    {
+        [Fact] //Her test edeceğimiz methoda vermemiz gerekiyor.
+        public void AddTest()
+        {
+            //Arrange
+            // Verileri Initialize ettiğim ilk adımdır.
+
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act
+            // Test edeceğimiz methodu çalıtırdığımız adımdır.
+
+            var total = calculator.add(a, b);
+
+            //Assert
+            // Test sonucunun beklenen değer ile eşleştiğini doğruladığımız adımdır.
+
+            Assert.Equal(total, a + b);
+
+        }
+    }
+}
